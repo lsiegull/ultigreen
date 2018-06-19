ca20d7cbdfa9453cca7bd09fdf645d7bb3e108cc
Isabella <irplonk@gatech.edu>
2018-06-19 17:40:28 -0400

    Add jdbc template

--- src/main/java/ultigreen/demo/controller/FoodController.java
+++ src/main/java/ultigreen/demo/controller/FoodController.java
@@ -3,6 +3,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,7 +18,13 @@ public class FoodController {
     private FoodService foodService;
 
     @PostMapping(path="/create")
-    public ResponseEntity<Double> create(@RequestParam Food[] foodItems) {
-        return new ResponseEntity<>(foodService.createService(foodItems), HttpStatus.CREATED);
+    public ResponseEntity<String> create(@RequestParam Food[] items) {
+        foodService.insertIntoDatabase(items);
+        return new ResponseEntity<>("Added food items to database", HttpStatus.CREATED);
+    }
+
+    @GetMapping(path="/calculate")
+    public ResponseEntity<Double> calculate() {
+        return new ResponseEntity<Double>(foodService.calculateCarbonFootprint(), HttpStatus.OK);
     }
 }
--- src/main/java/ultigreen/demo/services/FoodService.java
+++ src/main/java/ultigreen/demo/services/FoodService.java
@@ -3,20 +3,50 @@ package ultigreen.demo.services;
 import org.springframework.stereotype.Service;
 import ultigreen.demo.domain.FoodInfo;
 import ultigreen.demo.domain.Food;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Service
 public class FoodService {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     private static final int DAYS_IN_WEEK = 7;
     private static final int WEEKS_IN_YR = 52;
 
-    public double createService(Food[] foodItems) {
+    public void insertIntoDatabase(Food[] items) {
+        String list = "";
+        for (int i = 0; i < items.length - 1; i++) {
+            list += items[i].getServings();
+        }
+        list += items[items.length - 1];
+        List<Object[]> splitUpFood = Arrays.asList(list).stream().map(str -> str.split(" ")).collect(Collectors.toList());
+        String sql = "INSERT INTO food(";
+        for (int i = 0; i < items.length - 1; i++) {
+            sql += items[i].getName() + ", ";
+        }
+        sql += items[items.length - 1] + ") values (";
+        for (int i = 0; i < items.length - 1; i++) {
+            sql += "?, ";
+        }
+        sql += "?)";
+        jdbcTemplate.batchUpdate(sql, splitUpFood);
+    }
+
+    public double calculateCarbonFootprint() {
+        // Need to retreive items from db
         double totalDailyCarbonFootprint = 0;
-        for (Food foodItem : foodItems) {
-            String name = foodItem.getName().toUpperCase();
+        for (Food item : items) {
+            String name = item.getName().toUpperCase();
             FoodInfo info = FoodInfo.valueOf(name);
             if (info != null) {
-                totalDailyCarbonFootprint += info.calculateCarbonFootprint(foodItem.getServings());
+                totalDailyCarbonFootprint += info.calculateCarbonFootprint(item.getServings());
             }
         }
         return totalDailyCarbonFootprint * DAYS_IN_WEEK * WEEKS_IN_YR;
