c04473294f4d689815003dde1979d4ee112ae715
Sabrina Liu <sliu.owl68@yahoo.com>
2018-06-19 08:58:14 -0400

    Make questionnairePage template and applied to Transportation

--- client/src/app/questionnaire-page/questionnaire-page.component.ts
+++ client/src/app/questionnaire-page/questionnaire-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {QuestionItem} from '../questionItem';
 
 @Component({
   selector: 'app-questionnaire-page',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./questionnaire-page.component.css']
 })
 export class QuestionnairePageComponent implements OnInit {
+  @Input() title: string;
+  @Input() questionItems: QuestionItem[];
+  @Input() prevPage: string;
+  @Input() nextPage: string;
+
+  selectedQ: QuestionItem;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(selectedQ: QuestionItem): void {
+    this.selectedQ = selectedQ;
+  }
+
+  ngOnClick(isPrevious: boolean): void {
+    if (isPrevious) { //previous button
+      alert("Previous screen");
+    }else { //next button
+      alert("Next screen");
+
+    }
   }
 
 }
--- client/src/app/transportation/transportation.component.ts
+++ client/src/app/transportation/transportation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TransportationQuestion } from '../transportationQuestion';
+import { QuestionItem } from '../questionItem';
 
 @Component({
   selector: 'app-transportation',
@@ -7,30 +7,17 @@ import { TransportationQuestion } from '../transportationQuestion';
   styleUrls: ['./transportation.component.css']
 })
 export class TransportationComponent implements OnInit {
-  transportationQuestions: TransportationQuestion[] = [
+  transportationQuestions: QuestionItem[] = [
     {id: 1, question: "How far away in miles do you live from work?", answer: "12"},
     {id: 2, question: "What is the mileage of your car?", answer: "40"},
     {id: 3, question: "How many people do you carpool with?", answer: "5"},
     {id: 4, question: "How many times/ week do you carpool to and from work?", answer: "10"}
   ];
-  selectedQ: TransportationQuestion;
+  selectedQ: QuestionItem;
   
   constructor() { }
 
   ngOnInit() {
-  }
-
-  onSelect(transportationQ: TransportationQuestion): void {
-    this.selectedQ = transportationQ;
-  }
-
-  ngOnClick(isPrevious: boolean): void {
-    if (isPrevious) { //previous button
-      alert("Previous screen");
-    }else { //next button
-      alert("Next screen");
-
-    }
   }
 
 }
