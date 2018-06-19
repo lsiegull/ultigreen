7cac37323b75202c74e367fa94a48aa4c5c2bb53
Isabella <irplonk@gatech.edu>
2018-06-19 11:59:29 -0400

    Connect login to backend

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 ﻿import { Component, OnInit } from '@angular/core';
-import { first } from 'rxjs/operators';
 
 import { User } from '../models';
 import { UserService } from '../services';
--- client/src/app/services/authentication.service.ts
+++ client/src/app/services/authentication.service.ts
@@ -8,16 +8,17 @@ export class AuthenticationService {
     constructor(private http: HttpClient) { }
 
     login(username: string, password: string) {
-        return this.http.post<any>('/api/authenticate', { username: username, password: password })
-            .pipe(map(user => {
-                // login successful if there's a jwt token in the response
-                if (user && user.token) {
-                    // store user details and jwt token in local storage to keep user logged in between page refreshes
-                    localStorage.setItem('currentUser', JSON.stringify(user));
-                }
-
-                return user;
-            }));
+        return this.http.get<any>('http://localhost:8080/users/login', {
+            params: {
+                username: username,
+                password: password 
+            }
+        }).pipe(map(user => {
+            if (user) {
+                localStorage.setItem('currentUser', JSON.stringify(user.username));
+            }
+            return user;
+        }));
     }
 
     logout() {
