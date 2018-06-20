57e3a1eb763af65ff3eb75392b893cf3184bc5c7
Samuel Shapiro <samshapiro13@gmail.com>
2018-06-19 20:21:46 -0400

    Added endpoint for transportation footprint

---
+++ client/src/app/services/transportation.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { RequestOptions } from '@angular/http';
+import { map } from 'rxjs/operators';
+
+import { User } from '../models';
+
+@Injectable()
+export class TransportationService {
+    constructor(private http: HttpClient) { }
+
+    getTransportationFootprint(user: User) {
+        return this.http.get('http://localhost:8080/transportation/getTransportationCO2/' + user, { // fix endpoint
+            params: {}
+        }).pipe(map(result => {
+            return result
+        }));
+    }
+}
--- client/src/app/transportation/transportation.component.ts
+++ client/src/app/transportation/transportation.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { TransportationService } from '../services/transportation.service'
+import { User } from '../models';
+import { first } from 'rxjs/operators';
 
 @Component({templateUrl: 'transportation.component.html'})
 export class TransportationComponent implements OnInit {
 
-    constructor() {
-    }
+    private transportationFootprint
+    private currentUser: User
+
+    constructor(
+        private transportationService: TransportationService) { }
 
     ngOnInit() {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        this.transportationService.getTransportationFootprint(this.currentUser)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.transportationFootprint = data
+                },
+                error => {
+                    this.transportationFootprint = "Uncalculated"
+                });
     }
 }
